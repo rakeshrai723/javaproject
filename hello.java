ddbf7c51be0eb1c533a23d73eee6a26936a11512
rakeshrai723 <58048849+rakeshrai723@users.noreply.github.com>
2020-01-15 18:48:48 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,14 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+public class Hello{
+
+     public static void main(String[] args){
+	 
+	      for(int i=1;i<=10;i++){
+		  
+		        System.out.ptintln("Hello World..."+i);
+				
+				}
+				
+		}
+		
+}
 	
